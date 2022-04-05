0e74dcc8110fc078652f8990e93098dd52bc1723
Alex <alexbachurin91@gmail.com>
2022-04-05 06:15:17 +0300

    initial setup of app + Router setup

---
+++ src/components/AppRouter/AppRouter.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { Routes, Route } from "react-router-dom";
+
+const AppRouter: FC = () => {
+	return (
+		<Routes>
+			<Route path="/" />
+		</Routes>
+	);
+};
+
+export default AppRouter;
