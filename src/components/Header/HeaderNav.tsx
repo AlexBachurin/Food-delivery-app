9a636f46e7e3d803fa572a41fb83528154c075bf
Alex <alexbachurin91@gmail.com>
2022-04-07 12:08:56 +0300

    added motion frame to nav

--- src/components/Header/HeaderNav.tsx
+++ src/components/Header/HeaderNav.tsx
@@ -1,9 +1,15 @@
+import { motion } from "framer-motion";
 import React, { FC } from "react";
 import { Link } from "react-router-dom";
 import links from "../../utils/links";
 const HeaderNav: FC = () => {
 	return (
-		<ul className="flex md:flex-row lg:flex-row flex-col  h-full items-start md:items-center justify-start md:justify-between pt-16 pl-10 md:pt-0 md:pl-0 gap-10 lg:gap-24 md:gap-12 md:text-[16px] text-[20px] pr-16">
+		<motion.ul
+			initial={{ opacity: 0, x: 200 }}
+			animate={{ opacity: 1, x: 0 }}
+			exit={{ opacity: 0, x: 200 }}
+			className="flex md:flex-row lg:flex-row flex-col  h-full items-start md:items-center justify-start md:justify-between pt-16 pl-10 md:pt-0 md:pl-0 gap-10 lg:gap-24 md:gap-12 md:text-[16px] text-[20px] pr-16"
+		>
 			{links.map(({ id, path, name }) => {
 				return (
 					<Link
@@ -15,7 +21,7 @@ const HeaderNav: FC = () => {
 					</Link>
 				);
 			})}
-		</ul>
+		</motion.ul>
 	);
 };
 
